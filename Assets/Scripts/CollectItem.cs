80fd89c027a390a9b398b0b39ec371c59d566e24
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-03-12 00:22:07 +0100

    UI

--- Assets/Scripts/CollectItem.cs
+++ Assets/Scripts/CollectItem.cs
@@ -1,19 +1,103 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectItem : MonoBehaviour
 {
     public GameObject player;
     public float distanceThreshold = 1f;
-    public static int SebraneKusy = 0;
+
+    [SerializeField]
+    private Sprite podstava;
+
+    [SerializeField]
+    private Sprite podstavaDark;
+
+    [SerializeField]
+    private Sprite raketa;
+
+    [SerializeField]
+    private Sprite raketaDark;
+
+    [SerializeField]
+    private Sprite cannon;
+
+    [SerializeField]
+    private Sprite cannonDark;
+
+    [SerializeField]
+    private GameObject podstavaObj;
+
+    [SerializeField]
+    private GameObject raketaObj;
+
+    [SerializeField]
+    private GameObject cannonObj;
+
+    void Start()
+    {
+        Podstava(false);
+        Raketa(false);
+        Cannon(false);
+    }
+
+    public void Raketa(bool status)
+    {
+        if (status)
+        {
+            raketaObj.GetComponent<Image>().sprite = raketa;
+        }
+        else
+        {
+            raketaObj.GetComponent<Image>().sprite = raketaDark;
+        }
+    }
+    public void Podstava(bool status)
+    {
+        if (status)
+        {
+            podstavaObj.GetComponent<Image>().sprite = podstava;
+        }
+        else
+        {
+            podstavaObj.GetComponent<Image>().sprite = podstavaDark;
+        }
+    }
+    public void Cannon(bool status)
+    {
+        if (status)
+        {
+            cannonObj.GetComponent<Image>().sprite = cannon;
+        }
+        else
+        {
+            cannonObj.GetComponent<Image>().sprite = cannonDark;
+        }
+    }
 
     private void Update()
     {
         if (Vector3.Distance(transform.position, player.transform.position) < distanceThreshold)
         {
+            if (gameObject.name == "StartingMechanism5.1(Clone)")
+            {
+                Podstava(true);
+                Debug.Log("podstava");
+            }
+            else if(gameObject.name == "StartingMechanism5.2(Clone)")
+            {
+                Cannon(true);
+                Debug.Log("canon");
+            }
+            else if (gameObject.name == "StartingMechanism5.3(Clone)")
+            {
+                Raketa(true);
+                Debug.Log("raketa");
+            }
+                
             Destroy(gameObject);
-            SebraneKusy++;
+            
         }
     }
 }
