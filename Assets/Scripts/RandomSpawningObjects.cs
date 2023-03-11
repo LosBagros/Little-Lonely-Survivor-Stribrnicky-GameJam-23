aeecc0a7ea9d7e7e7dd56fcdaf4ee3784fc38601
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-03-11 21:46:45 +0100

    pro ondyho

---
+++ Assets/Scripts/RandomSpawningObjects.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawningObjects : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] objectToSpawn;
+
+    void Start()
+    {
+        List<Transform> availableSpawnPoints = new List<Transform>(spawnPoints);
+        for (int i = 0; i < 3; i++)
+        {
+            int randomIndex = Random.Range(0, availableSpawnPoints.Count);
+            Transform spawnPoint = availableSpawnPoints[randomIndex];
+            Instantiate(objectToSpawn[i], spawnPoint.position, spawnPoint.rotation);
+            availableSpawnPoints.RemoveAt(randomIndex);
+        }
+    }
+}
