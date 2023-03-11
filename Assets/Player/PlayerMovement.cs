197f998137d65c967c0b7ef1d23c605e8b797ce6
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-03-11 02:50:30 +0100

    player movement and animations

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float rotateSpeed = 3.0f;
+    public GameObject planet;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private Rigidbody planetRigidbody;
+
+    private Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        planetRigidbody = planet.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        // pøidáme gravitaci k pohybu hráèe
+        moveDirection.y -= 9.8f * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        if (moveDirection != Vector3.zero)
+        {
+            //animator.SetBool("isWalking", true);
+
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * rotateSpeed);
+
+            Vector3 groundNormal = transform.position - planet.transform.position;
+            transform.rotation = Quaternion.FromToRotation(transform.up, groundNormal) * transform.rotation;
+        }
+        else
+        {
+            //animator.SetBool("isWalking", false);
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("isWalking", true);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                animator.SetBool("isRunning", true);
+                speed = 8.0f;
+            }
+            else
+            {
+                animator.SetBool("isRunning", false);
+                speed = 6.0f;
+            }
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        planetRigidbody.AddTorque(transform.right * -Input.GetAxis("Horizontal") * rotateSpeed);
+        planetRigidbody.AddTorque(transform.forward * Input.GetAxis("Vertical") * rotateSpeed);
+    }
+}
